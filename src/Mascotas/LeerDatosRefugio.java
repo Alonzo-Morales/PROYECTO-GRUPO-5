96df44a73355cfea3182d29c7565cbc734cc1475
Reyes209c <132095662+Reyes209c@users.noreply.github.com>
2024-05-31 00:53:17 -0600

    Carlos Gómez

---
+++ src/Mascotas/LeerDatosRefugio.java
@@ -0,0 +1,34 @@
+package Mascotas;
+
+public class LeerDatosRefugio 
+{
+    public LeerDatosRefugio()
+    {}
+
+    public boolean getLeerNombre(String nom)
+    {
+        try
+        {
+            return nom.matches("^([a-z]+[ ]?[a-z]+[ ]?[a-z]+)$"); 
+        } 
+        catch(Exception e)
+        {
+            //JOptionPane.showMessageDialog(null,"Existe un error: "+e,"!! ERROR !!", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }    
+    }
+    
+    public boolean getLeerTelefono(String telefono)
+    {
+        try
+        {
+            return telefono.matches("^([0-9]{10})$"); 
+        } 
+        catch(Exception e)
+        {
+            //JOptionPane.showMessageDialog(null,"Existe un error: "+e,"!! ERROR !!", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }    
+    }
+    
+}
